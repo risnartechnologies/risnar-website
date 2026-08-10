5e9b375114896c82734c5003fecb2fc20ec883c6
RISNAR Technologies <risnartechnologies@gmail.com>
2026-08-10 05:47:35 +0530

    Filter WhatsApp templates to show only approved templates

--- lib/meta/templates.ts
+++ lib/meta/templates.ts
@@ -2,8 +2,7 @@ import { metaRequest } from "./client";
 
 export async function fetchTemplates() {
   const wabaId =
-    process.env
-      .META_WABA_ID;
+    process.env.META_WABA_ID;
 
   if (!wabaId) {
     throw new Error(
@@ -11,7 +10,17 @@ export async function fetchTemplates() {
     );
   }
 
-  return metaRequest(
-    `/${wabaId}/message_templates`
-  );
+  const response =
+    await metaRequest(
+      `/${wabaId}/message_templates`
+    );
+
+  return {
+    ...response,
+
+    data: (response.data ?? []).filter(
+      (template: any) =>
+        template.status === "APPROVED"
+    ),
+  };
 }
